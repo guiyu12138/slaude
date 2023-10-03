cbb307bb4f28a6663807a61ad1b1ad1072499179
guiyu12138 <143297122+guiyu12138@users.noreply.github.com>
2023-10-04 00:28:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    TOKEN: "xoxc-xxxx",
-    COOKIE: "xoxd-xxxx",
-    TEAM_ID: "workspacename",
-    CHANNEL: "C1234D4567S",
-    CLAUDE_USER: "U1234A568BC",
+    TOKEN: "xoxc-5940966605172-5951155469073-5931874443766-43a857739c88a221080c46fb47505e72e305c2dcf786650078192cafc3552bf2",
+    COOKIE: "xoxd-KL68q4dcM%2F6w%2FJuuuo%2FaDhjmxfHgs6edWQNqcvoPFDzp5t44K4lw7ekYSfPscBY4N6u%2FyRQtPua%2FPp%2B9kwlOGN%2FggedY%2FIQjBT4XPC5h3cOxyv%2BOTwMkjiMmR69dOL7IiEFSqSMwSAff4U5%2BBnaU0wI6OozkAyrpTT5H2HUIqUWeJ6FZrxweB1rB00DIpHsd2eLeE42SUHU%3D",
+    TEAM_ID: "w1695415296-bgp137044",
+    CHANNEL: "C05SU10K15K",
+    CLAUDE_USER: "U05F0G8UVEJ",
 
     MAINPROMPT_LAST: false, // Will try to move the main prompt (main + nsfw prompt from SillyTavern) to the bottom, similar to a jailbreak
     MAINPROMPT_AS_PING: false, // Will use the main prompt as the ping message, if this true the PING_MESSAGE setting is ignored. Overrides MAINPROMPT_LAST.
